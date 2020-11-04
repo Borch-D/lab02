8053c054bb99a7df09aa0cb778c6689bc3c4971f
Borch_D <divdl@mail.ru>
2020-11-04 19:24:04 +0300

    Add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char const *argv[])
 {
-	std::string name;
-	std::cin >> name;
-	std::cout << "Hello world from " << name << std::endl;
-	return 0;
+	std::string name; // объявили строку 
+	std::cin >> name; // считали строку
+	std::cout << "Hello world from " << name << std::endl; // вывели на экран
+	return 0; // вернули значение
 }
